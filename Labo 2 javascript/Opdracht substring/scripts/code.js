ebf86c13e7e98bb8c0d3302d1f848e2c4d83d62c
AgentX009X <elias.botte@student.vives.be>
2023-02-20 11:41:18 +0100

    Javascript labo 02 correctie

--- Labo 2 javascript/Opdracht substring/scripts/code.js
+++ Labo 2 javascript/Opdracht substring/scripts/code.js
@@ -4,9 +4,9 @@ const setup = () => {
     substringbutton.addEventListener("click", substring);
 }
 const substring = () => {
-    let eerstegrens = document.getElementById("eerstegrens");
-    let tweedegrens = document.getElementById("tweedegrens");
-    let txtInput = document.getElementById("txtInput");
+    let eerstegrens = document.getElementById("eerstegrens").value;
+    let tweedegrens = document.getElementById("tweedegrens").value;
+    let txtInput = document.getElementById("txtInput").value;
     let result = txtInput.substring(eerstegrens, tweedegrens);
     let txtOutput = document.getElementById("txtOutput");
     txtOutput.innerHTML = result;
